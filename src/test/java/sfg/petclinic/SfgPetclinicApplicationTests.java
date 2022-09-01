4daf784ba2a86b37f437be17e345fa2e670c4b5d
ajay16oct <ajeet.ambujagmail.com>
2022-09-01 09:43:07 +0530

    Initial Commit

---
+++ src/test/java/sfg/petclinic/SfgPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package sfg.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
